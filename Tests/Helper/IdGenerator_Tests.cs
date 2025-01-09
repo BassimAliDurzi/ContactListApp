a4da622965583d3d3edb7e977137eacaf67e5bf5
Bassim Durzi <bassim.ali.durzi@gmail.com>
2025-01-09 11:23:55 +0100

    Build unit test IdGenerator_Tests

---
+++ Tests/Helper/IdGenerator_Tests.cs
@@ -0,0 +1,18 @@
+﻿using Business.Helper;
+
+namespace Tests.Helper;
+
+public class IdGenerator_Tests
+{
+    [Fact]
+    public void Generate_ShouldReturnGuidAsString()
+    {
+        // Act
+        var result = IdGenerator.Generate();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(Guid.TryParse(result, out _));
+    }
+
+}
